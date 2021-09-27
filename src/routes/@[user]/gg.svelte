68109903c796ac3f84b7231dc5425a2356b16c70
kangdongmandoo <kangdongmandoo@gmail.com>
2021-09-27 22:54:56 +0900

    Fixed Bugs

---
+++ src/routes/@[user]/gg.svelte
@@ -0,0 +1,16 @@
+<script context="module">
+	export async function load({ page }) {
+		return {
+			props: {
+				user: page.params.user
+			}
+		};
+	}
+</script>
+
+<script>
+	export let user;
+	if (typeof window !== 'undefined') {
+		window.location.replace(`/@${user}`);
+	}
+</script>
