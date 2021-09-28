551ebda798a7d8a0dc9c50535d95e1da428632da
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-09-28 11:32:42 +0900

    Update manifest.js

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -1,14 +1,15 @@
 const c = [
-	() => import("..\\..\\..\\src\\routes\\__layout.svelte"),
-	() => import("..\\..\\..\\src\\routes\\__error.svelte"),
-	() => import("..\\..\\..\\src\\routes\\index.svelte"),
-	() => import("..\\..\\..\\src\\routes\\account\\signout.svelte"),
-	() => import("..\\..\\..\\src\\routes\\account\\signin.svelte"),
-	() => import("..\\..\\..\\src\\routes\\account\\signup\\index.svelte"),
-	() => import("..\\..\\..\\src\\routes\\policy\\index.svelte"),
-	() => import("..\\..\\..\\src\\routes\\api\\dashboard.svelte"),
-	() => import("..\\..\\..\\src\\routes\\@[user]\\index.svelte"),
-	() => import("..\\..\\..\\src\\routes\\@[user]\\gg.svelte")
+	() => import("../../../src/routes/__layout.svelte"),
+	() => import("../../../src/routes/__error.svelte"),
+	() => import("../../../src/routes/index.svelte"),
+	() => import("../../../src/routes/account/signout.svelte"),
+	() => import("../../../src/routes/account/signin.svelte"),
+	() => import("../../../src/routes/account/signup/index.svelte"),
+	() => import("../../../src/routes/policy/index.svelte"),
+	() => import("../../../src/routes/api/dashboard.svelte"),
+	() => import("../../../src/routes/@[user]/index.svelte"),
+	() => import("../../../src/routes/@[user]/share/[post].svelte"),
+	() => import("../../../src/routes/@[user]/gg.svelte")
 ];
 
 const d = decodeURIComponent;
@@ -35,8 +36,11 @@ export const routes = [
 	// src/routes/@[user]/index.svelte
 	[/^\/@([^/]+?)\/?$/, [c[0], c[8]], [c[1]], (m) => ({ user: d(m[1])})],
 
+	// src/routes/@[user]/share/[post].svelte
+	[/^\/@([^/]+?)\/share\/([^/]+?)\/?$/, [c[0], c[9]], [c[1]], (m) => ({ user: d(m[1]), post: d(m[2])})],
+
 	// src/routes/@[user]/gg.svelte
-	[/^\/@([^/]+?)\/gg\/?$/, [c[0], c[9]], [c[1]], (m) => ({ user: d(m[1])})]
+	[/^\/@([^/]+?)\/gg\/?$/, [c[0], c[10]], [c[1]], (m) => ({ user: d(m[1])})]
 ];
 
 // we import the root layout/error components eagerly, so that
