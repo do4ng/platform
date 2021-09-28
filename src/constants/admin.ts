f09043ee19e02bc0ea47ec364e7ce003f537e3de
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-09-28 11:43:43 +0900

    Update admin.ts

--- src/constants/admin.ts
+++ src/constants/admin.ts
@@ -1,1 +1,1 @@
-export const ADMIN: string[] = ['0bd0b4cd-e571-4a96-9b7a-8adcdf7f0200'];
+export const ADMIN: string[] = ['0bd0b4cd-e571-4a96-9b7a-8adcdf7f0200', 'e2b5c7e3-be95-4f61-a9ec-f296cfa314b7'];
