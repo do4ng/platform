beac8af39ad9af6e05821d9aee9f9d1ff641f1c7
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-10-01 08:04:41 +0900

    Delete .svelte-kit directory

--- .svelte-kit/output/client/_app/chunks/account-7912d45b.js
+++
@@ -1,1 +0,0 @@
-import{s as r}from"./db-b47507ac.js";import{f as t}from"./fetch-10917518.js";async function a(r){return await t(`/account/profile?id=${r}`)}async function n(){const t=r.auth.user();return{user:t,profile:await a(t?t.id:null)}}export{n as g};
--- .svelte-kit/output/client/_app/chunks/fetch-10917518.js
+++
@@ -1,1 +0,0 @@
-async function t(t){let e;return await fetch(`https://kdmplatform.herokuapp.com${t}`).then((t=>t.json())).then((t=>e=t)),e}export{t as f};
