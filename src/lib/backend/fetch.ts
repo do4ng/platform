f3bcbf1249ecf5b39bbc45bba6212b77dd95d5e6
kangdongmandoo <kangdongmandoo@gmail.com>
2021-10-03 23:39:27 +0900

    Update fetch.ts

--- src/lib/backend/fetch.ts
+++ src/lib/backend/fetch.ts
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export default async function fetchApiServer(address: string): Promise<any> {
 	let r: any;
+	/*
 	await fetch(`https://kdmplatform.herokuapp.com${address}`)
+		.then((res) => res.json())
+		.then((data) => (r = data));
+		*/
+	await fetch(`http://localhost:4000${address}`)
 		.then((res) => res.json())
 		.then((data) => (r = data));
 	return r;
