53727165ba0d1dd7f2262e73d2e38e69cc16b19d
kangdongmandoo <kangdongmandoo@gmail.com>
2021-10-03 23:39:38 +0900

    Create get.ts

---
+++ src/lib/time/get.ts
@@ -0,0 +1,4 @@
+export default function getTime(): string {
+	const date = new Date();
+	return `${date.getUTCFullYear()}-${date.getUTCMonth()}-${date.getUTCDate()} ${date.getUTCHours()}:${date.getUTCMinutes()}:${date.getUTCSeconds()}`;
+}
