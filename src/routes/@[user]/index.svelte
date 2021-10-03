d2f0c37be61023422bc45b4e71c394dcf170b9cc
kangdongmandoo <kangdongmandoo@gmail.com>
2021-10-03 23:39:40 +0900

    Update index.svelte

--- src/routes/@[user]/index.svelte
+++ src/routes/@[user]/index.svelte
@@ -14,6 +14,9 @@
 	import fetchApiServer from '$lib/backend/fetch';
 	import { ADMIN } from '../../constants/admin';
 	import Badge from '../../components/badge.svelte';
+	import getAgo from '$lib/time/ago';
+	import linkParser from '$lib/parser/link';
+	import userParser from '$lib/parser/user';
 
 	export let user;
 
@@ -21,30 +24,41 @@
 
 	async function loadUser() {
 		const res = await fetchApiServer(`/account/profilebynick?nickname=${user}`);
-		// console.log(res);
-		if (typeof window !== undefined) {
-			document.title = `@${user}`;
-		}
+
 		return res;
 	}
 	async function upload() {
 		if (inputtext && !uploading) {
-			uploading = true;
-			if (typeof window !== undefined) {
-				document.title = `@${user} - uploading..`;
-			}
 			const u = await getProfile();
+			if (u.profile.id === null) {
+				window.location.href = '/account/signin';
+			}
+			uploading = true;
 			await fetchApiServer(`/upload/${user}?by=${await u.user.id}&nickname=${u.profile.nickname}&content=${inputtext}`);
 
+			if (typeof window !== 'undefined') {
+				window.location.reload();
+			}
+		}
+	}
+	async function follow() {
+		const u = await getProfile();
+		if (u.profile.id === null) {
+			window.location.href = '/account/signin';
+		}
+		const res = await fetchApiServer(`/follow/${user}?by=${await u.user.id}`);
+		if (typeof window !== 'undefined') {
 			window.location.reload();
 		}
+
+		return res;
 	}
 	let promise = loadUser();
 	let inputtext = '';
 </script>
 
 <svelte:head>
-	<title>Connecting..</title>
+	<title>{user} - platform</title>
 </svelte:head>
 
 {#await promise then res}
@@ -63,31 +77,39 @@
 					{res.about}
 				</div>
 			</div>
-			<div class="post">
-				<div class="form">
-					<input bind:value={inputtext} type="text" placeholder="{res.nickname}님에게 전하고 싶은 말을 입력하세요." />
-					<button on:click={upload}>게시</button>
-				</div>
-				{#each res.data.reverse() as p, index}
-					<div class="card" id="post-{res.data.length - index}-{p.nick}">
-						<div class="title">
-							<a href="/@{p.nick}/gg" class="no-deco">{p.nick}</a>
-							{#if res.data.length - index === 1}
-								<Badge># {res.data.length - index}</Badge>
-							{:else if res.data.length - index === 2}
-								<Badge># {res.data.length - index}</Badge>
-							{:else if res.data.length - index === 3}
-								<Badge># {res.data.length - index}</Badge>
-							{/if}
-							{#if p.nick === res.nickname}
-								<Badge>소유자</Badge>
-							{/if}
+			<div class="main-content">
+				<div class="show">
+					<div class="show-title">소개</div>
+					<div class="show-content">
+						<div class="show-follow">
+							팔로워 <span class="show-bold">{res.follower.length}</span>
 						</div>
-						<div class="content">
-							{p.content}
+						<div class="show-follow">
+							팔로잉 <span class="show-bold">{res.following.length}</span>
 						</div>
+						<div class="show-follow">
+							전체 글 <span class="show-bold">{res.data.length}</span>
+						</div>
+						<button on:click={follow} class="show-button">Follow</button>
 					</div>
-				{/each}
+				</div>
+				<div class="post">
+					<div class="form">
+						<input bind:value={inputtext} type="text" placeholder="{res.nickname}님에게 전하고 싶은 말을 입력하세요." />
+						<button on:click={upload}>게시</button>
+					</div>
+					{#each res.data.reverse() as p, index}
+						<div class="card" id="post-{res.data.length - index}-{p.nick}">
+							<div class="title">
+								<a href="/@{p.nick}/gg" class="no-deco">{p.nick}</a>{' '}
+								<span class="time">{getAgo(p.time)}</span>
+							</div>
+							<div class="content">
+								{@html userParser(linkParser(p.content))}
+							</div>
+						</div>
+					{/each}
+				</div>
 			</div>
 		</div>
 	{/if}
@@ -112,24 +134,73 @@
 				color: var(--theme-gray);
 			}
 		}
-		.post {
+		.main-content {
+			display: flex;
 			margin-top: 25px;
-			width: 55%;
+			width: 75%;
 			margin: 35px auto;
-			.card {
-				box-shadow: 0 0 2.2px var(--theme-nav-color);
-				width: 100%;
-				height: auto;
-				padding: 17px 17px;
-				border: var(--theme-nav-color) solid 1.5px;
-				border-radius: 7.5px;
-				margin: 35px 0;
-				.title {
-					font-size: 1rem;
+			word-break: break-all;
+			.show {
+				margin-top: 60px;
+				margin-right: 5%;
+				width: 30%;
+				height: 220px;
+				border-radius: 15px;
+				border: 0.1px solid var(--theme-nav-color);
+				box-shadow: 0 0 1px var(--theme-nav-color);
+				padding: 15px 22.5px;
+				.show-title {
+					font-weight: bold;
+					font-size: 1.225rem;
+				}
+				.show-content {
+					margin-top: 15px;
+				}
+				.show-follow {
+					margin-bottom: 5px;
+				}
+				.show-bold {
 					font-weight: bold;
 				}
-				.content {
-					margin-top: 7px;
+				.show-button {
+					margin-top: 18px;
+					width: 100%;
+					height: 35px;
+					border-radius: 7px;
+					outline: none;
+					border: none;
+					background-color: var(--theme-nav-color);
+					transition: all ease 0.2s 0s;
+				}
+				.show-button:hover {
+					border: none;
+					background-color: var(--theme-nav-color-hover);
+					transition: all ease 0.2s 0s;
+				}
+			}
+			.post {
+				width: 70%;
+				.card {
+					box-shadow: 0 0 2.2px var(--theme-nav-color);
+					width: 100%;
+					height: auto;
+					padding: 17px 17px;
+					border: var(--theme-nav-color) solid 1.5px;
+					border-radius: 7.5px;
+					margin: 35px 0;
+					.title {
+						font-size: 1rem;
+						font-weight: bold;
+						.time {
+							font-weight: lighter;
+							font-size: 0.75rem;
+							color: var(--theme-gray);
+							padding-left: 2px;
+						}
+					}
+					.content {
+						margin-top: 7px;
+					}
 				}
 			}
 		}
