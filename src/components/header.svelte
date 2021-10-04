f9a807b3b11cf9254f778054ed3364630ed9bf33
kangdongmandoo <kangdongmandoo@gmail.com>
2021-10-04 22:26:30 +0900

    Update header.svelte

--- src/components/header.svelte
+++ src/components/header.svelte
@@ -1,8 +1,27 @@
+<script>
+	function theme() {
+		const classlist = document.querySelector('body').classList;
+		if (!localStorage.getItem('theme')) {
+			localStorage.setItem('theme', 'light');
+		}
+		if (localStorage.getItem('theme') === 'dark') {
+			localStorage.setItem('theme', 'light');
+			classlist.remove(`theme-dark`);
+			classlist.add(`theme-light`);
+		} else if (localStorage.getItem('theme') === 'light') {
+			localStorage.setItem('theme', 'dark');
+			classlist.remove(`theme-light`);
+			classlist.add(`theme-dark`);
+		}
+	}
+</script>
+
 <div class="header">
 	<slot />
 	<div class="header-main">
 		<div class="header-content">
 			<div class="header-title"><a href="/" class="no-deco">platform</a></div>
+			<div class="theme-d"><i class="fi fi-sr-moon" on:click={theme} /></div>
 		</div>
 	</div>
 </div>
@@ -23,11 +42,15 @@
 	}
 	.header-content {
 		max-width: 1280px;
+		display: flex;
 		width: 90%;
 		margin: 0 auto;
 	}
 	.header-title {
 		font-weight: bold;
 		font-size: 1.175rem;
+	}
+	.theme-d {
+		margin-left: auto;
 	}
 </style>
