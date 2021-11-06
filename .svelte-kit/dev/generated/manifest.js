7ce4b906643c538444880e37b15a9ab298921a38
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-11-06 10:40:07 +0900

    Update manifest.js

--- .svelte-kit/dev/generated/manifest.js
+++ .svelte-kit/dev/generated/manifest.js
@@ -2,8 +2,6 @@ const c = [
 	() => import("..\\..\\..\\src\\routes\\__layout.svelte"),
 	() => import("..\\..\\..\\src\\routes\\__error.svelte"),
 	() => import("..\\..\\..\\src\\routes\\index.svelte"),
-	() => import("..\\..\\..\\src\\routes\\account\\setting\\index.svelte"),
-	() => import("..\\..\\..\\src\\routes\\account\\setting\\[page].svelte"),
 	() => import("..\\..\\..\\src\\routes\\account\\signout.svelte"),
 	() => import("..\\..\\..\\src\\routes\\account\\signin.svelte"),
 	() => import("..\\..\\..\\src\\routes\\account\\signup\\index.svelte"),
@@ -19,32 +17,26 @@ export const routes = [
 	// src/routes/index.svelte
 	[/^\/$/, [c[0], c[2]], [c[1]]],
 
-	// src/routes/account/setting/index.svelte
-	[/^\/account\/setting\/?$/, [c[0], c[3]], [c[1]]],
-
-	// src/routes/account/setting/[page].svelte
-	[/^\/account\/setting\/([^/]+?)\/?$/, [c[0], c[4]], [c[1]], (m) => ({ page: d(m[1])})],
-
 	// src/routes/account/signout.svelte
-	[/^\/account\/signout\/?$/, [c[0], c[5]], [c[1]]],
+	[/^\/account\/signout\/?$/, [c[0], c[3]], [c[1]]],
 
 	// src/routes/account/signin.svelte
-	[/^\/account\/signin\/?$/, [c[0], c[6]], [c[1]]],
+	[/^\/account\/signin\/?$/, [c[0], c[4]], [c[1]]],
 
 	// src/routes/account/signup/index.svelte
-	[/^\/account\/signup\/?$/, [c[0], c[7]], [c[1]]],
+	[/^\/account\/signup\/?$/, [c[0], c[5]], [c[1]]],
 
 	// src/routes/policy/index.svelte
-	[/^\/policy\/?$/, [c[0], c[8]], [c[1]]],
+	[/^\/policy\/?$/, [c[0], c[6]], [c[1]]],
 
 	// src/routes/api/dashboard.svelte
-	[/^\/api\/dashboard\/?$/, [c[0], c[9]], [c[1]]],
+	[/^\/api\/dashboard\/?$/, [c[0], c[7]], [c[1]]],
 
 	// src/routes/@[user]/index.svelte
-	[/^\/@([^/]+?)\/?$/, [c[0], c[10]], [c[1]], (m) => ({ user: d(m[1])})],
+	[/^\/@([^/]+?)\/?$/, [c[0], c[8]], [c[1]], (m) => ({ user: d(m[1])})],
 
 	// src/routes/@[user]/gg.svelte
-	[/^\/@([^/]+?)\/gg\/?$/, [c[0], c[11]], [c[1]], (m) => ({ user: d(m[1])})]
+	[/^\/@([^/]+?)\/gg\/?$/, [c[0], c[9]], [c[1]], (m) => ({ user: d(m[1])})]
 ];
 
 // we import the root layout/error components eagerly, so that
