d712d4358b661cfb0ece08a06d6e3b5dfe8c18c4
r.ehdals <57896501+kangdongmandoo@users.noreply.github.com>
2021-12-11 14:11:25 +0900

    Delete db.ts

--- src/lib/db.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-	'https://sqlyyrbnygivtlqxumub.supabase.co',
-	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzOTEzOTEzNiwiZXhwIjoxOTU0NzE1MTM2fQ.7FhCRi4IiODZivrBnTKfhRVYEjkLBpZ0ubhcCj1nVyI'
-);
-
-export default supabase;
